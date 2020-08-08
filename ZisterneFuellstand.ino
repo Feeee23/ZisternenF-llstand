5732156de115155f904f4ea939d934eded12a6eb
Feeee23 <63659321+Feeee23@users.noreply.github.com>
2020-08-08 11:56:35 +0200

    Add files via upload

--- ZisterneFuellstand.ino
+++ ZisterneFuellstand.ino
@@ -1,10 +1,14 @@
 #include <Stepper.h>
-
-// include the library code:
 #include <LiquidCrystal.h>
 #include<SR04.h>
 #include<Wire.h>
 #include<DS3231.h>
+#include<SPI.h>
+#include<SD.h>
+//*********************************************************allg Declaration
+//File erstellen
+File myFile;
+const int CS=53;
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
@@ -19,7 +23,6 @@ RTCDateTime dt;
 int Schalter=4; //Pin für Schalter  Def
 int FStand=0;
 int L=0;
-char dat[16];
 int Backlight=13; //HIntergundlicht über Pin 13 Steuern
 
 //*****************************************Setup
@@ -31,9 +34,22 @@ void setup() {
   pinMode(Backlight, OUTPUT);
   pinMode(Schalter, INPUT);
   digitalWrite(Schalter, HIGH);
-  }
+  if(!SD.begin()){
+    digitalWrite(Backlight, HIGH);
+    lcd.print("initalization failed");
+    delay(1000);
+    digitalWrite(Backlight, LOW);
+    }
+ }
 //**********************************************Ausgabe
 void Ausgabe(int hoehe, int liter){
+   myFile=SD.open("Daten.txt", FILE_WRITE);
+   if(!myFile){
+    digitalWrite(Backlight, HIGH);
+    lcd.print("Error open File");
+    delay(1000);
+    digitalWrite(Backlight, LOW);
+   }
    digitalWrite(Backlight, HIGH);
    
    lcd.clear();
@@ -42,57 +58,60 @@ void Ausgabe(int hoehe, int liter){
    lcd.setCursor(0, 0);
    if(dt.day<10){
     lcd.print("0");
+    myFile.print("0");
    }
    lcd.print(dt.day); lcd.print(".");
+   myFile.print(dt.day); myFile.print(".");
     if(dt.month<10){
     lcd.print("0");
+    myFile.print("0");
    }
    lcd.print(dt.month); lcd.print(".");
+   myFile.print(dt.month); myFile.print(".");
    lcd.print(dt.year); lcd.setCursor(0, 1);
+   myFile.print(dt.year); myFile.print("  ");
     if(dt.hour<10){
     lcd.print("0");
+    myFile.print("0");
    }
    lcd.print(dt.hour); lcd.print(":");
+   myFile.print(dt.hour);myFile.print(":");
    if(dt.minute<10){
     lcd.print("0");
+    myFile.print("0");
    }
    lcd.print(dt.minute);
+   myFile.print(dt.minute); myFile.print("  ");
    delay(3000);
   
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Fuellstand:");
+  myFile.print("Fuellstand: ");
   lcd.setCursor(0, 1);
   lcd.print(hoehe);
+  myFile.print(hoehe);
   lcd.setCursor(5, 1);
   lcd.print("cm");
+  myFile.print(" cm, ");
   delay(3000);
    
    lcd.clear();
    lcd.setCursor(0, 0);
    lcd.print("entspricht ca.:");
+   myFile.print("entspricht ca.: ");
    lcd.setCursor(0, 1);
    lcd.print(liter);
+   myFile.print(liter);
    lcd.setCursor(5, 1);   
    lcd.print("l");
+   myFile.println(" l");
   delay(3000);
 
+  myFile.close();
  lcd.clear();
  digitalWrite(Backlight, LOW);
 }
-//*****************************************Datum
-/*void Datum(char* dat){
-  dt=clock.getDateTime();
-  itoa(dt.day,dat[0], 10);
-  dat[2]=".";
-  itoa(dt.month,dat[4],10);
-  dat[5]=".";
-  itoa(dt.year,dat[6],10);
-  dat[10]=" ";
-  itoa(dt.hour,dat[11],10);
-  dat[13]=":";
-  itoa(dt.minute,dat[14],10);
-  }*/
 //**************************************Sensor
 int Sensor(){
   int A=sr04.Distance();
@@ -122,11 +141,37 @@ void loop() {
     }
   
 }
-
+/*Verbindungen
+ * D4-> Taster zum Einschalten weiter auf GND
+ * D5-> Ultraschall TRIG
+ * D6-> Ultraschall ECHO
+ * D7-> Display RS (4v.l.)
+ * D8-> Dispaly E (6v.l.)
+ * D9-> Display D4 (11v.l.)
+ * D10-> Display D5 (12v.l.)
+ * D11-> Display D6 (13v.l.)
+ * D12-> Display D7 (14v.l.)
+ * D13-> Display A (15v.l.)
+ *       Display K (16v.l.)->GND
+ *       Display VSS (1v.l.)->GND
+ *       Dispaly VDD (2v.l.)->5V
+ *       Dispaly V0 (3v.l.)->Poti->rest plus minus
+ *       Display RW (5v.l.)->GND
+ * D50-> SD-Karte MISO 
+ * D51-> SD-Karte MOSI
+ * D52-> SD-Karte SCK
+ * D53-> SD-Karte CS
+ *       SD-Karte Vcc
+ *       SD-Karte GND
+ * SCL-> RTC SCL 
+ * SDA-> RTC SDA
+ *       RTC GND
+ *       RTC VCC
+ *       RTC SQW->nix
+ */
 
 /*ToDO
 Schalter über LOW laufen lassen
-Speichern auf karte
 Maximale Tiefe ändern in Sensor
 Formern ändern in Recner
 arduino schlafen legen
